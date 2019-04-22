e629ebfb6cd173f45bf10b908c64f4111e520bbd
brandongatlin <brandongatlin1981@me.com>
2019-04-22 15:51:51 -0500

    randomizing and rendering to DOM

--- README.md
+++ README.md
@@ -8,3 +8,18 @@
 - modal to select type/language of game and start
 - quit button
 - number correct in a row
+
+### state
+
+- language
+- copy of randomize pix array
+
+* load array of pix into state
+* randomize it (maybe before loading it)
+* call populate card from the state
+
+#### functions needed:
+
+- read array, and randomize it, then load into state
+- start game (call others)
+- check if winner
--- client/src/components/card/index.js
+++ client/src/components/card/index.js
@@ -1,11 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Pic from "../pic";
 
 const pics = require("../../foods");
 
 const Card = props => {
-  return pics.map(pic => {
+  const [randomizedPix, setRandomized] = useState([]);
+
+  const randomizer = data => {
+    let i = data.length - 1;
+    while (i > 0) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = data[i];
+      data[i] = data[j];
+      data[j] = temp;
+      i--;
+    }
+
+    useEffect(() => {
+      return setRandomized(data);
+    });
+  };
+
+  // import start, then use it to call randomizer/start game & randomizer
+  randomizer(pics);
+
+  return randomizedPix.map(pic => {
     return (
       <Pic key={pic.name} className='bingoPic' alt={pic.name} src={pic.url} />
     );
