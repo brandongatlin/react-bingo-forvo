bd041db734a3a043181c8e252e222ea7222e6a1d
brandongatlin <brandongatlin1981@me.com>
2020-06-11 23:30:06 -0500

    now calling in correct order and checking too

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import "./App.css";
 import jsonp from 'jsonp';
 import shuffle from 'lodash.shuffle';
@@ -18,39 +18,39 @@ const App = props => {
   const [randomizedPix, setRandomized] = useState([]);
   const [callOrder, setCallOrder] = useState([]);
   let [called, setCalled] = useState(0);
+  let [currentAnswer, setCurrentAnswer] = useState('');
   let [srcMp3, setSrcMp3] = useState('');
   let [srcOgg, setSrcOgg] = useState('');
   const [correct, setCorrect] = useState([]);
   const [wrong, setWrong] = useState([]);
+ 
 
   const callNextWord = () => {
-    console.log('nxt')
+
     const apiKey = 'a1947295bd2a7535393c3c3df3d666b0';
     const currentWord = callOrder[called].name;
+    setCurrentAnswer(callOrder[called].id);
 
         jsonp(`https://apifree.forvo.com/action/word-pronunciations/format/json/word/${currentWord}/id_lang_speak/39/order/date-desc/limit/5/key/${apiKey}`, null, (err, data)=> {
       const mp3Src = data.items[0].pathmp3;
       const oggSrc = data.items[0].pathogg;
       setSrcMp3(mp3Src);
       setSrcOgg(oggSrc);
+      setCalled(called + 1);
 
     });
-      
   }
 
   const validate = (e) => {
-    const answer = callOrder[called].id;
     const chosen = e.target.id;
-    console.log(answer, chosen)
 
-    if(answer === chosen){
-      setCorrect([...correct, answer]);
+    if(currentAnswer === chosen){
+      setCorrect([...correct, currentAnswer]);
     } else {
       setWrong([...wrong, chosen]);
     }
-      setCalled(called + 1);
 
-    if(called <= 24){
+    if(called < callOrder.length){
       callNextWord();
     }
   }
@@ -60,15 +60,18 @@ const App = props => {
     
     return (
       <div id='gameBoard'>
-        <div>STATS PLACEHOLDER</div>
+        <div onClick={callNextWord} >CLICK 2 BEGIN</div>
         <Card 
-        id='bingoCard' 
-        order={randomizedPix} 
-        correct={correct} 
-        wrong={wrong} 
-        validate={validate}/>
+          id='bingoCard' 
+          order={randomizedPix} 
+          correct={correct} 
+          wrong={wrong} 
+          validate={validate}/>
         <div>
-           <ReactAudioPlayer autoPlay src={srcOgg}/>
+           <ReactAudioPlayer 
+            autoPlay 
+            src={srcOgg}
+           />
         </div>
       </div>
     )
@@ -82,13 +85,16 @@ const App = props => {
           onClick={(e)=> {
             setGameOn(true);
             setRandomized(shuffle(pics));
-            setCallOrder(shuffle(pics));
+            setCallOrder(shuffle(pics))
           }}>
             Start
         </button>
         <BilingualOptions 
-        language={language} 
-        category={category} changeLang={setLanguage} changeCat={setCategory}/>
+          language={language} 
+          category={category} 
+          changeLang={setLanguage} 
+          changeCat={setCategory}
+        />
       </div>
     );
 
